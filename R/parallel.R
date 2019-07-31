7c2c5976d759243197d8050a7634d1a9a6f404da
Florian Privé <florian.prive.21@gmail.com>
2019-07-31 15:40:29 +0200

    reexport parallel

---
+++ R/parallel.R
@@ -0,0 +1,15 @@
+################################################################################
+
+#' @importFrom parallel makeCluster
+#' @export
+parallel::makeCluster
+
+#' @importFrom doParallel registerDoParallel
+#' @export
+doParallel::registerDoParallel
+
+#' @importFrom parallel stopCluster
+#' @export
+parallel::stopCluster
+
+################################################################################

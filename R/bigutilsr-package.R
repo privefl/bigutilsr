b5f139fcd974cb2c9d6fd7b59c22c7cb6f4a2850
Florian Privé <florian.prive.21@gmail.com>
2019-08-01 08:57:16 +0200

    do not reexport parallel

--- R/bigutilsr-package.R
+++ R/bigutilsr-package.R
@@ -1,4 +1,7 @@
 #' @useDynLib bigutilsr, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+#' @importFrom parallel makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
+#' @import foreach
 #' @keywords internal
 "_PACKAGE"

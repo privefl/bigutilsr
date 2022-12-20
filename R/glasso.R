8a73d6339ef63a85cdb48ebe31aafed93d3a9482
Florian Privé <florian.prive.21@gmail.com>
2022-12-20 09:49:39 +0100

    new function regul_lasso()

---
+++ R/glasso.R
@@ -0,0 +1,45 @@
+#' Regularization with the graphical lasso
+#'
+#' Use the graphical lasso algorithm to regularize a square symmetric matrix
+#' (e.g. a covariance or correlation matrix) by assuming that its inverse has
+#' many zeros.
+#'
+#' @param mat A square symmetric matrix.
+#' @param lambda Strength of regularization. It needs to be scaled with `mat`.
+#'   It should also be the maximum difference between the two matrices.
+#' @param maxiter_outer Maximum number of iterations of the outer loop.
+#'   Default is 200.
+#' @param maxiter_lasso Maximum number of iterations of each lasso solver.
+#'   Default is 200.
+#' @param tol Tolerance for assessing convergence. Default is 1e-4 and it needs
+#'   to be scaled with `mat`.
+#' @param verbose Whether to print iterations and differences. Default is FALSE.
+#'
+#' @return The regularized matrix, where the diagonal should be the same and
+#'   zeros should be kept as well. It also returns the `lambda` used as an attribute.
+#' @export
+#'
+#' @examples
+#' (cov <- cov(iris[1:4]))
+#' lambda <- 1 / sqrt(nrow(iris))
+#' (cov_regul <- regul_glasso(cov, lambda))
+regul_glasso <- function(mat,
+                         lambda,
+                         maxiter_outer = 200,
+                         maxiter_lasso = 200,
+                         tol = 1e-4,
+                         verbose = FALSE) {
+  repeat {
+
+    new_mat <- try(
+      glasso(mat, lambda, maxiter_outer, maxiter_lasso, tol, verbose),
+      silent = TRUE)
+
+    if (inherits(new_mat, "try-error")) {
+      bigassertr::warning2("Divergence! Increasing lambda and retrying..")
+      lambda <- lambda * 1.5
+    } else break
+  }
+
+  structure(new_mat, lambda = lambda, dimnames = dimnames(mat))
+}
---
+++ tests/testthat/test-glasso.R
@@ -0,0 +1,24 @@
+################################################################################
+
+context("GEO_MEDIAN")
+
+################################################################################
+
+test_that("regul_glasso() works", {
+
+  cov <- cov(iris[1:4])
+  lambda <- 1 / sqrt(nrow(iris))
+  cov_regul <- regul_glasso(cov, lambda)
+  expect_equal(diag(cov_regul), diag(cov))
+  expect_lte(max(abs(cov_regul - cov)), lambda * 1.0001)
+
+  cov_regul2 <- regul_glasso(cov * 10, lambda * 10, tol = 1e-3)
+  expect_equal(cov_regul2, structure(cov_regul * 10, lambda = lambda * 10),
+               tolerance = 1e-4)
+
+  cov_regul3 <- regul_glasso(cov / 10, lambda / 10, tol = 1e-5)
+  expect_equal(cov_regul3, structure(cov_regul / 10, lambda = lambda / 10),
+               tolerance = 1e-4)
+})
+
+################################################################################
